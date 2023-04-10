5be6ca9186be00ef28eec04f9c43185437801241
Damien <damienambient@gmail.com>
2023-04-10 23:17:40 +0300

    pictures auto change

--- src/js/main.js
+++ src/js/main.js
@@ -6,23 +6,25 @@ import checkTextInputs from "./modules/checkTextInputs";
 import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
 import filter from "./modules/filter";
+import pictureSize from "./modules/pictureSize";
 
 window.addEventListener("DOMContentLoaded", () => {
-  "use strict";
+    "use strict";
 
-  modals();
-  slider(
-    ".feedback-slider-item",
-    "horizontal",
-    ".main-prev-btn",
-    ".main-next-btn"
-  );
-  slider(".main-slider-item", "vertical");
-  forms();
-  mask('[name="phone"]');
-  checkTextInputs('[name="name"]');
-  checkTextInputs('[name="message"]');
-  showMoreStyles(".button-styles", "#styles .row");
-  calc("#size", "#material", "#options", ".promocode", ".calc-price");
-  filter();
+    modals();
+    slider(
+        ".feedback-slider-item",
+        "horizontal",
+        ".main-prev-btn",
+        ".main-next-btn",
+    );
+    slider(".main-slider-item", "vertical");
+    forms();
+    mask('[name="phone"]');
+    checkTextInputs('[name="name"]');
+    checkTextInputs('[name="message"]');
+    showMoreStyles(".button-styles", "#styles .row");
+    calc("#size", "#material", "#options", ".promocode", ".calc-price");
+    filter();
+    pictureSize(".sizes-block");
 });
